d2afc02b595c95b5b8d0a535a154f464afb3bf65
LukasSteib <lukas.steib@student.kit.edu>
2022-10-30 15:32:04 +0000

    Hochladen

---
+++ Vorversuch/.ipynb_checkpoints/parameters_task_3-checkpoint.py
@@ -0,0 +1,15 @@
+"""
+Parameters required for task 3.
+
+The only parameter required for the model of the mathematical pendulum of task 
+3 is the distance l of the center of mass of the smartphone from the pivotal 
+point of the pendulum. It is a derived quantity, since it implies knowledge of 
+the center of mass of the smartphone, but its derivation is not too evolved. 
+The value of l is given in SI units  
+"""
+
+l = 0.6285                   # Distance of the center of mass of the smartphone from the pivotal point of the pendulum (in m)
+l_UPPER = 0.6280             # +uncertainty
+l_LOWER = 0.6290             # -uncertainty
+
+__version__ = 1.0
